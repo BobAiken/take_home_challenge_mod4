84f06ea8bc6a035052bf771c4c80216fee37ac32
Robert Aiken <robertsiraaiken@gmail.com>
2023-04-12 15:35:09 -0500

    Add error handling when articles cannot be fetched

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import apiCall from '../APIcalls';
 import Homepage from '../Homepage/homepage';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Article from '../Article/Article';
 
@@ -10,10 +10,12 @@ function App() {
 
   const [section, setSection] = useState("home")
   const [articles, setArticles] = useState([])
+  const [error, setError] = useState(false)
 
   useEffect(()=>{
     apiCall(section)
     .then(data=>setArticles(data.results))
+    .catch(error=>setError(true))
   },[section])
 
   return (
--- src/Homepage/homepage.js
+++ src/Homepage/homepage.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
 import "./homepage.css"
 import { Link } from "react-router-dom";
 
 export default function Homepage({articles}) {
 
-  const articleCards = articles.map((article,index) => {
-    return(
-      <Link to={article.title}>
-        <div className="article-card" key={index}>
-          <div>{article.title}</div>
-          {article.multimedia && <img className="article-image" src={article.multimedia[2].url}/>}
-        </div>
-      </Link>
-    )
-  })
+  const [articleCards, setArticleCards] = useState()
+
+  useEffect(()=>{
+    if(articles){
+      setArticleCards(articles.map((article,index) => {  
+        return(
+          <Link to={article.title}>
+            <div className="article-card" key={index}>
+              <div>{article.title}</div>
+              {article.multimedia && <img className="article-image" src={article.multimedia[2].url}/>}
+            </div>
+          </Link>
+        )
+    }))
+    } else {
+      setArticleCards(<>Something has gone wrong...</>)
+    }
+  },[articles])
 
   return (
     <section className="article-card-container">
